f13edaee23887579191002dc4af50a5bf3231fe6
kuktaekim <ktkim@slac.stanford.edu>
2023-07-10 00:10:37 -0700

    Update pva_callback.py for the mpvaMonitor.

--- pva_callback.py
+++ pva_callback.py
@@ -1,12 +1,15 @@
 from p4p.client.thread import Context
 import time
- 
+import sys
+
+
 def my_value_processor(value):
-	""" Adds three to the value and prints it. """
-	print(f'After adding 3, the PV has a value of: {value+3}')
-						 
+	""" Print updated PV. """
+	print(f'NEW: {sys.argv[1]}      {value}')
+
+
 ctxt = Context('pva')
-subscription = ctxt.monitor('your_username:pva_temp', my_value_processor)
+subscription = ctxt.monitor(sys.argv[1], my_value_processor)
 
 while True:
 	time.sleep(0.1)
