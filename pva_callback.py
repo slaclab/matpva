2f5a217142a7cb90518398d572375fedca67de3b
kuktaekim <ktkim@slac.stanford.edu>
2023-07-11 11:19:23 -0700

    Added a commment for the Python script

--- pva_callback.py
+++ pva_callback.py
@@ -2,9 +2,14 @@ from p4p.client.thread import Context
 import time
 import sys
 
+# This python script is developed to monitor PVs using the p4p module.
+
 
 def my_value_processor(value):
-	""" Print updated PV. """
+	"""Print updated PV.
+
+	:param value: the PV name
+	"""
 	print(f'NEW: {sys.argv[1]}      {value}')
 
 
